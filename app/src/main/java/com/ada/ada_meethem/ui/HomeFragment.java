29bf05397b6be4edf4c14049926ac0749f0f25c5
Abel Busto Dopazo <UO284262@uniovi.es>
2023-10-21 12:31:53 +0200

    First NAV approach

---
+++ app/src/main/java/com/ada/ada_meethem/ui/HomeFragment.java
@@ -0,0 +1,62 @@
+package com.ada.ada_meethem.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ada.ada_meethem.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "text";
+
+    private String text;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param text Parameter 1.
+     * @return A new instance of fragment HomeFragment.
+     */
+    public static HomeFragment newInstance(String text) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, text);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            text = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root= inflater.inflate(R.layout.fragment_home, container, false);
+        TextView tvArgumento = root.findViewById(R.id.text_home);
+        tvArgumento.setText(text);
+        return root;
+    }
+}
