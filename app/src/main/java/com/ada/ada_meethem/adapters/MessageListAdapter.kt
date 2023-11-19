83a91c180fb31c15a1a56864fa340a748322baf3
Abel Busto Dopazo <UO284262@uniovi.es>
2023-11-19 12:15:31 +0100

    Chat fix + estetic things

--- app/src/main/java/com/ada/ada_meethem/adapters/MessageListAdapter.kt
+++ app/src/main/java/com/ada/ada_meethem/adapters/MessageListAdapter.kt
@@ -1,71 +1,77 @@
-package com.ada.ada_meethem.adapters;
+package com.ada.ada_meethem.adapters
 
-import android.content.Context;
-import android.text.format.DateFormat;
-import android.widget.BaseAdapter;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import java.util.List;
+import android.content.Context
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.ada.ada_meethem.R
+import com.ada.ada_meethem.database.entities.ChatMessage
+import com.google.firebase.auth.FirebaseAuth
 
-import com.ada.ada_meethem.R;
-import com.ada.ada_meethem.database.entities.ChatMessage;
+class MessageListAdapter(
 
-import java.util.List;
+    private val context: Context,
+    private val listaMessages: List<ChatMessage>,
+    private val colorsArray: IntArray
+) : BaseAdapter() {
 
-public class MessageListAdapter extends BaseAdapter {
-        private List<ChatMessage> listaMessages;
-        private Context context;
+    private val TYPEOWN : Int = 0
+    private val TYPEOTHER : Int = 1
+    override fun getCount(): Int {
+        return listaMessages.size
+    }
 
-        private int[] colorsArray;
+    override fun getItem(position: Int): Any {
+        return listaMessages[position]
+    }
 
-        public MessageListAdapter(Context context, List<ChatMessage> listaMessages,int[] colorsArray) {
-            this.context = context;
-            this.listaMessages = listaMessages;
-            this.colorsArray = colorsArray;
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getViewTypeCount(): Int {
+        return 2
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val userPhoneNumber = (getItem(position) as ChatMessage).messageUser.substring(3)
+        return if (userPhoneNumber == FirebaseAuth.getInstance().currentUser!!.phoneNumber!!.substring(3))
+            TYPEOWN else TYPEOTHER
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val viewType = getItemViewType(position)
+        val chatMessage = getItem(position) as ChatMessage
+        val userPhoneNumber = chatMessage.messageUser.substring(3)
+        var view = convertView
+        if (view == null) {
+            val inflater =
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = inflater.inflate(if (viewType == TYPEOWN) R.layout.own_message else R.layout.message, null)
         }
 
-        @Override
-        public int getCount() {
-            return listaMessages.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return listaMessages.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = convertView;
-
-            if (view == null) {
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(R.layout.message, null);
-            }
-
-            // Obtén el usuario actual
-            ChatMessage chatMessage = listaMessages.get(position);
-            String userPhoneNumber = chatMessage.getMessageUser().substring(3);
-            int color = colorsArray[Integer.parseInt(userPhoneNumber)%18];
-            TextView messageText = (TextView)view.findViewById(R.id.message_text);
-            TextView messageUser = (TextView)view.findViewById(R.id.message_user);
-            TextView messageTime = (TextView)view.findViewById(R.id.message_time);
-            // Set their text
-            messageText.setText(chatMessage.getMessageText());
-            messageUser.setTextColor(color);
-            messageUser.setText(userPhoneNumber);
-            // Format the date before showing it
-            messageTime.setText(DateFormat.format("HH:mm (dd-MM-yyyy)",
-                    chatMessage.getMessageTime()));
+        // Obtén el usuario actual
 
 
-            return view;
-        }
+        val color = colorsArray[userPhoneNumber.toInt() % 18]
+        val messageText = view!!.findViewById<View>(
+            if (viewType == TYPEOWN) R.id.message_text_own else R.id.message_text_own) as TextView
+        val messageUser = view.findViewById<View>(
+            if (viewType == TYPEOWN) R.id.message_user_own else R.id.message_user_own) as TextView
+        val messageTime = view.findViewById<View>(
+            if (viewType == TYPEOWN) R.id.message_time_own else R.id.message_time_own) as TextView
+        // Set their text
+        messageText.text = chatMessage.messageText
+        messageUser.setTextColor(color)
+        messageUser.text = if(viewType == TYPEOWN) "You" else userPhoneNumber
+        // Format the date before showing it
+        messageTime.text = DateFormat.format(
+            "EEE HH:mm",
+            chatMessage.messageTime
+        )
+        return view
+    }
 }
