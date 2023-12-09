ebeb9040c2c1047927840c0e19faa8645618b6ed
UO282492 <uo282492@uniovi.es>
2023-12-10 00:13:08 +0100

    Create plan view finished

--- app/src/main/java/com/ada/ada_meethem/modelo/Contact.kt
+++ app/src/main/java/com/ada/ada_meethem/modelo/Contact.kt
@@ -1,7 +1,35 @@
 package com.ada.ada_meethem.modelo
 
+import android.os.Parcel
+import android.os.Parcelable
+
 // Representa un contacto del móvil
 data class Contact(
-    val number: String,
-    val username: String
-)
+    val number: String?,
+    val username: String?
+): Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(number)
+        parcel.writeString(username)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Contact> {
+        override fun createFromParcel(parcel: Parcel): Contact {
+            return Contact(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Contact?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
